7eb5cb70616127c30c8b933908e8cd50475c2e94
Lalit R Nagpal <lalit.r.nagpal@gmail.com>
2021-01-20 18:58:14 +0530

    Component Template Examples

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 1. first-app-ngmodule
 1. blank-template
 1. angular-components
+1. templates-example
 1. styling
 1. 
 
--- component-templates/e2e/src/app.e2e-spec.ts
+++ component-templates/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('template-examples app is running!');
+    expect(await page.getTitleText()).toEqual('component-templates app is running!');
   });
 
   afterEach(async () => {
